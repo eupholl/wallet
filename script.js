a8f13d07d3e4b5afc0b63220097d1dabc4a1fafe
eupholl <karxqm@mail.ru>
2026-08-22 19:38:03 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+<div class="card-subtext" id="count-888">найдено 0 штук</div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <span class="card-title">Usernames</span>
+                <span class="card-value" id="val-user">0 GRAM ≈ $0</span>
+            </div>
+            <div class="card-subtext" id="count-user">найдено 0 штук</div>
+        </div>
+
+        <div class="card">
+            <div class="card-header">
+                <span class="card-title">NFT Коллекции</span>
+                <span class="card-value" id="val-nft">0 GRAM ≈ $0</span>
+            </div>
+            <div class="card-subtext" id="count-nft">найдено 0 штук</div>
+        </div>
+
+        <div class="card scam-card">
+            <div class="card-header">
+                <span class="card-title scam-title">Scam</span>
+                <span class="card-value" style="color: #ff453a;" id="val-scam">Скрыто</span>
+            </div>
+            <div class="card-subtext" style="color: #ff453a;" id="count-scam">найдено 0 штук</div>
+        </div>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
